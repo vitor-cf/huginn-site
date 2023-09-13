a02098598e2c8756ddb2b28718057e703942d8e9
Vitor Ferreira <vitorcarv.ferreira@gmail.com>
2023-09-12 22:33:04 -0300

    primeira previsa site huginn

---
+++ layouts/background.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="back bg-[#02071B] h-[100vh] w-full">
+        <header class="flex justify-center p-6">
+            <div class="flex justify-between items-center w-[98%]">
+                <div>
+                    <h1 class="text-[32px] text-white"><strong class="text-[#848EBD]">ATLÉTICA</strong> HUGINN</h1>
+                </div>
+                <div>
+                    <ul class="flex space-x-5">
+                        <li class="text-[24px] text-white">LOJA</li>
+                        <li class="text-[24px] text-white">PROSEL</li>
+                        <li class="text-[24px] text-white">EVENTOS</li>
+                        <li class="text-[24px] text-white">DIRETORIA</li>
+                        <li class="text-[24px] text-white">MODALIDADES</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+            <slot/>
+
+    </div>
+</template>
+<style scoped>
+.back {
+    background-image:url('assets/imgs/logo.svg') ;
+    background-size: 100%;
+    background-repeat: no-repeat;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <NuxtLayout name="background">
+        <!-- Home -->
+        <main>
+            <div class="flex justify-evenly">
+                <div class="w-[40%]">
+                    <img src="/img/huguinho1.png" />
+                </div>
+                <div class="flex flex-col w-[35%] space-y-5 justify-center">
+                    <div class="w-5/6">
+                        <h2 class="text-[#848EBD] text-[20px] font-bold">CONEXÃO</h2>
+                        <p class="text-white font-bold">Somos mais do que apenas uma atlética, somos uma família unida pela paixão, pelo conhecimento, inovação e
+                            superação de desafios.</p>
+                    </div>
+                    <div class="w-5/6">
+                        <h3 class="text-[#848EBD] text-[20px] font-bold">PRINCÍPIOS</h3>
+                        <p class="text-white font-bold">Estamos comprometidos em cultivar um ambiente onde ideias inovadoras floresçam e onde todos possam
+                            contribuir para um futuro mais brilhante.</p>
+                    </div>
+                    <div class="w-5/6">
+                        <h4 class="text-[#848EBD] text-[20px] font-bold">MASCOTE</h4>
+                        <p class="text-white font-bold">Huginn é um dos dois corvos que voam pelo mundo todos os dias para coletar informações e notícias para o
+                            deus nórdico Odin. A escolha do nome Huginn para uma atlética de engenharias é significativa, pois
+                            sugere uma conexão com a busca pelo conhecimento, pela inovação e pelo pensamento crítico, atributos
+                            essenciais para qualquer engenheiro ou engenheira de sucesso.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+</NuxtLayout></template>
